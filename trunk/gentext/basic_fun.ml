50c680e9c0dfd24d9d045536f35a7768c3b0ac80
sbleune <sbleune@d24a7aa5-49ac-d203-54fb-132c510c8066>
2010-06-19 02:02:28 +0000

    Basic function (managing dictionaries)

--- trunk/gentext/basic_fun.ml
+++ trunk/gentext/basic_fun.ml
@@ -1,82 +1,56 @@
 #use "types.ml";;
 
-let noms      = ref Node [] and
-    verbes    = ref Node [] and
-    articles  = ref Node [] and
-    pronoms   = ref Node [] and
-    adjectifs = ref Node []
-;;
+(* Nos dictionnaires : plus ou moins des arbres de décision *)
 
-let nettoyer = fun s -> Str.global_replace (Str.regexp "_") " " s;;
+let noms      = ref (Node []) and
+    verbes    = ref (Node []) and
+    articles  = ref (Node []) and
+    pronoms   = ref (Node []) and
+    adjectifs = ref (Node [])
 
-let rec take n xs = match n with
-	| 0     -> List.hd xs
-	| n + 1 -> take n (List.tl xs)
-;;
+(* Nettoyage des chaînes de caractères *)
 
-let rec add elem nom tree = match elem , tree with
-	| []     , Leaf xs -> Leaf (nom :: xs)
-	| n :: q , Node xs -> Node (add_list n q nom xs)
+let nettoyer = fun s -> Str.global_replace (Str.regexp "_") " " s
+
+(** Remplissage des dictionnaires : insertion dans un arbre de
+décision en suivant les indications données par une liste de choix.
+Chaque choix est un naturel indiquant le sous-arbre fils **)
+
+let rec lookup n xs = match n with
+	| 0 -> List.hd xs
+	| n -> lookup (n - 1) (List.tl xs)
+
+let rec insert_tree path value tree = match path , tree with
+	| []     , Leaf xs -> Leaf (value :: xs)
+	| n :: q , Node xs -> Node (insert_list n q value xs)
 	| _      , _       -> Leaf []
 
-and add_list n elem nom xs = match elem , xs with
-	| 0     , []      -> [add elem nom (Node [])]
-	| 0     , x :: xs -> (add elem nom x) :: xs
-	| n + 1 , []      -> (Node []) :: (add_list n elem nom [])
-	| n + 1 , x :: xs -> x :: (add_list n elem nom [])
-;;
+and insert_list index path value xs = match index , xs with
+	| 0 , []      -> [insert_tree path value (Node [])]
+	| 0 , x :: xs -> (insert_tree path value x) :: xs
+	| n , []      -> (Node []) :: (insert_list (n - 1) path value [])
+	| n , x :: xs -> x :: (insert_list (n - 1) path value xs)
 
-(*
-
-TODO: From here
-
-*)
+(** Spécifications : chaque arbre (profondeur, nombre de fils
+à chaque "étage", etc.) et les fonctions inserent donc les
+bons mots dans les bons arbres **)
 
 let add_nom nom espece chp_lex genre =
-    let nom     = nettoyer nom in
-    let new_nom =
-		{ espece  = espece;
-		mot     = nom;
-		chp_lex	= chp_lex; }
-    in noms.(genre) <- (M new_nom)::(noms.(genre))
-;;
+    let nom   = nettoyer nom in
+        noms := insert_tree [genre ; espece ; chp_lex] nom (!noms)
 
 let add_adjectif adjectif esp genre =
-    let adjectif = nettoyer adjectif in
-    let new_adj  =
-		{ espece = esp;
-		mot    = adjectif;
-		chp_lex = -1; }
-    in adjectifs.(genre) <- (M new_adj)::(adjectifs.(genre))
-;;
+    let adjectif   = nettoyer adjectif in
+        adjectifs := insert_tree [genre ; esp] adjectif (!adjectifs)
 
 let add_verbe verbe esp_suj esp_cibl chp_suj chp_cibl chp_lex =
-    let verbe    = nettoyer verbe in
-    let new_verb = {
-		verbe    = verbe;
-		esp_suj  = esp_suj;
-		esp_cibl = esp_cibl;
-		chp_suj  = chp_suj;
-		chp_cibl = chp_cibl;
-		chp_v    = chp_lex;
-		   }
-    in verbes := (V new_verb)::(!verbes)
-;;
+    let verbe   = nettoyer verbe in
+        verbes := insert_tree [esp_suj ; esp_cibl ; chp_lex ; chp_suj ; chp_cibl] verbe (!verbes)
 
 let add_article article genre =
-    let article = nettoyer article in
-    let new_art =
-		{ espece  = -1;
-		mot     = article;
-		chp_lex = -1; }
-    in articles.(genre) <- (M new_art)::(articles.(genre))
-;;
+    let article   = nettoyer article in
+    	articles := insert_tree [genre] article (!articles)
 
 let add_pronom pronom genre =
-    let pronom  = nettoyer pronom in
-    let new_pro =
-		{ espece  = -1;
-		mot     = pronom;
-		chp_lex = -1; }
-    in pronoms.(genre) <- (M new_pro)::(pronoms.(genre))
-;;
+    let pronom   = nettoyer pronom in
+    	pronoms := insert_tree [genre] pronom (!pronoms)
