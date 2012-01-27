c92869dcab5fa5e514722f45010802fc8f93c138
hugo.feree@gmail.com <hugo.feree@gmail.com@d24a7aa5-49ac-d203-54fb-132c510c8066>
2012-01-27 13:53:19 +0000

    check for doublons. Much much more efficient!

--- trunk/genealogy/gen.sh
+++ trunk/genealogy/gen.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
-parent() {
-	local id=$1
-	#get page
-	wget -q http://genealogy.math.ndsu.nodak.edu/id.php?id=$id
+# add id to the list (temp file) and check if it was already present
 
-	#get name
-	local temp=`cat id.php\?id\=$id  | grep '<title' | sed 's$<title>The Mathematics Genealogy Project - \(.*\)<\(.\)*$\1$g'`
-	father=$temp
-	if [[ "$son" == "" ]]; then
-		son=$father
-	else
-		echo "\"$father\" -> \"$son\";"
-		son=$father
-	fi
-	# test if there is an advisor
-	if [[ `cat id.php\?id\=$id | grep Advisor` == *Unknown*  ]]; then 
-		rm id.php\?id\=$id
-	else 
-		#find advisor id
-		local adv_id=`cat id.php\?id\=$id | grep Advisor | sed 's/\(.\)*id=\([0-9]*\)\(.\)*/\2/' `
-		local adv2_id=`cat id.php\?id\=$id | grep Advisor | sed "s/\(.\)*id=\([0-9]*\)\(.\)*id.php\(.\)*/\2/" `
-		rm id.php\?id\=$id
-		parent $adv_id
-		if [[ ${#adv2_id} -lt 10 && "$adv2_id" != "" ]]; then 
-			son=$temp
-			parent $adv2_id
+function check_list(){
+	#number of nodes
+	local n=`wc -l .list`
+	echo $1 >> .list
+	sort .list  | uniq > .list.tmp
+	mv -f .list.tmp .list
+	[[ `wc -l .list` == $n ]]
+}
+
+function parent() {
+	local id=$1
+	if(! check_list $id)
+		then
+		#get page
+		wget -q http://genealogy.math.ndsu.nodak.edu/id.php?id=$id
+
+		#get name
+		local temp=`cat id.php\?id\=$id  | grep '<title' | sed 's$<title>The Mathematics Genealogy Project - \(.*\)<\(.\)*$\1$g'`
+		father=$temp
+		if [[ "$son" == "" ]]; then
+			son=$father
+		else
+			echo "\"$father\" -> \"$son\";"
+			son=$father
+		fi
+		# test if there is an advisor
+		if [[ `cat id.php\?id\=$id | grep Advisor` == *Unknown*  ]]; then 
+			rm id.php\?id\=$id
+		else 
+			#find advisor id
+			local adv_id=`cat id.php\?id\=$id | grep Advisor | sed 's/\(.\)*id=\([0-9]*\)\(.\)*/\2/' `
+			local adv2_id=`cat id.php\?id\=$id | grep Advisor | sed "s/\(.\)*id=\([0-9]*\)\(.\)*id.php\(.\)*/\2/" `
+			rm id.php\?id\=$id
+			parent $adv_id
+			
+			# check for a second advisor
+			if [[ ${#adv2_id} -lt 10 && "$adv2_id" != "" ]]; then 
+				son=$temp
+				parent $adv2_id
+			fi
 		fi
 	fi
 }
 
+#initialize
+touch .list
 
-echo "strict digraph genealogy {"
+echo "digraph genealogy {"
 for id in $*; do
 		son=""
 	parent $id
 done
 echo "}"
+
+# clean up
+rm .list
